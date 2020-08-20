40dd002024f4d0fac986b46f56685fc1d5c1da22
Grace0Hud <gracein3d@gmail.com>
2020-08-19 17:14:08 -0700

    Sime constructors/ toString

--- src/MagicDriver.java
+++ src/MagicDriver.java
@@ -5,13 +5,16 @@ public class MagicDriver
         //create an object
         Dragon happy = new Dragon("Happy", "green", 20, 200, 5, 12.4, false, true);
         Dragon nobody = new Dragon();
+        Slime george = new Slime("George", 10, 15, 6, 20.35, true, "blue", true);
         //nobody.setName("Sad");
         //print out the object via toString
         System.out.println(happy.toString());
         System.out.println(nobody.toString());
+        System.out.println(george.toString());
         //System.out.println("Nobody has been given a name! It's " + nobody.getName());
-        nobody.setMight(10);
-        happy.setHealth(happy.getHealth() - nobody.fireBreath(2));
-        System.out.println(nobody.getName() + " is attacking " + happy.getName() + "! " + happy.getName() + " now has : " + happy.getHealth() + " health");
+        //nobody.setMight(10);
+        //happy.setHealth(happy.getHealth() - nobody.fireBreath(2));
+       // System.out.println(nobody.getName() + " is attacking " + happy.getName() + "! " + happy.getName() + " now has : " + happy.getHealth() + " health");
+
     }//end main method
 }//end driver class
--- src/Slime.java
+++ src/Slime.java
@@ -5,5 +5,32 @@ public class Slime extends Enemy
     boolean isPoisonous;
 
     //constructors
+    public Slime(String name, int might, int health, int movement, double speed, boolean isHostile, String color, boolean isPoisonous)
+    {
+        super(name, might, health, movement, speed, isHostile);
+        this.color = color;
+        this.isPoisonous = isPoisonous;
+    }//end full contr
+
+    public Slime()
+    {
+        super();
+        color = "green";
+        isPoisonous = false;
+    }//end default
+
+    //setters
+
+    //getters
+
+    //toString
+    public String toString()
+    {
+        String output = "---- SLIME ----";
+        output += super.toString();
+        output += "\nColor: " + color;
+        output += "\nisPoisonous: " + isPoisonous;
+        return output;
+    }//end toString
 
 }
