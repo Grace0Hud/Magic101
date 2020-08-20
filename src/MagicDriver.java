427e40761cd0cacee4a19fbb02a6173e6a9d2446
Grace0Hud <gracein3d@gmail.com>
2020-08-20 13:22:24 -0700

    Added two brain methods to monster class and added a list of enemies to driver

--- src/MagicDriver.java
+++ src/MagicDriver.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+
 public class MagicDriver
 {
     public static void main(String[] args)
@@ -6,22 +8,30 @@ public class MagicDriver
         Dragon happy = new Dragon("Happy", "green", 20, 200, 5, 12.4, false, true);
         Dragon nobody = new Dragon();
         Slime george = new Slime("George", 10, 15, 6, 20.35, true, "blue", true);
-        Slime nobody2 = new Slime();
-        Monster drac = new Monster("Drac", 15, 100, 5, 19.45, true, 3);
+        //Slime nobody2 = new Slime();
+        Monster drac = new Monster("Drac", 15, 100, 5, 19.45, true, 3, "air");
         Monster nobody3 = new Monster();
         Monster somebody = new Monster(4);
+        System.out.println(nobody3.compareTerrain());
+
+        Enemy[] enemies = {happy, george, drac};
+        for(Enemy e: enemies)
+        {
+            System.out.println(e.toString());
+            System.out.println();
+        }
         //nobody.setName("Sad");
         //print out the object via toString
        // System.out.println(happy.toString());
        // System.out.println(nobody.toString());
         //System.out.println(george.toString());
-        System.out.println(drac.toString());
+        //System.out.println(drac.toString());
         //System.out.println(nobody3.toString());
         //System.out.println(somebody.toString());
         //System.out.println("Nobody has been given a name! It's " + nobody.getName());
         //nobody.setMight(10);
         //drac.setLevel(5);
-        System.out.println(drac.toString());
+        //System.out.println(drac.toString());
         happy.setHealth(happy.getHealth() - drac.attack(2));
         System.out.println(drac.getName() + " is attacking " + happy.getName() + "! " + happy.getName() + " now has : " + happy.getHealth() + " health");
 
--- src/Monster.java
+++ src/Monster.java
@@ -2,23 +2,26 @@ public class Monster extends Enemy
 {
     //intstance variable
     int level;
-
+    String terrain;
     //constructors
-    public Monster(String name, int might, int health, int movement, double speed, boolean isHostile, int level)
+    public Monster(String name, int might, int health, int movement, double speed, boolean isHostile, int level, String terrain)
     {
         super(name, might, health, movement, speed, isHostile);
         this.level = level;
+        this.terrain = terrain;
     }//end full constructor
 
     public Monster(int level)
     {
         super();
         scale(level);
+        this.terrain = "land";
         this.level = level;
     }//creates default monster but scales up to level
     public Monster()
     {
         super();
+        this.terrain = "land";
         level = 0;
     }//end default constructor
 
@@ -58,6 +61,42 @@ public class Monster extends Enemy
         }//end if level is less
         //nothing happens if they are the same.
     }//end scale method
+
+    public String mightReact()
+    {
+        if(might < 10)
+        {
+            return "Weakling!";
+        }
+        else if(might >= 10 && might < 20)
+        {
+            return "Be warry...";
+        }
+        else //(might > 20)
+        {
+            return "OHHO... best stay away from that one..";
+        }
+    }//end might react
+
+    public String compareTerrain()
+    {
+        if(terrain.equalsIgnoreCase("land"))
+        {
+            return "You can run away";
+        }
+        else if(terrain.equalsIgnoreCase("water"))
+        {
+            return "You can swim away";
+        }
+        else if(terrain.equalsIgnoreCase("air"))
+        {
+            return "Bummer for you....";
+        }
+        else
+        {
+            return "invalid input";
+        }
+    }//compareTerrain
     //toString
     public String toString()
     {
