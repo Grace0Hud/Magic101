934511a579ce3edcfe39db036f35e8fd696b5ac4
Grace0Hud <gracein3d@gmail.com>
2020-08-24 17:02:16 -0700

    sorcerer constructors/toString

--- src/MagicDriver.java
+++ src/MagicDriver.java
@@ -6,19 +6,22 @@ public class MagicDriver
     {
         //create an object
         Dragon happy = new Dragon("Happy", "green", 20, 200, 5, 12.4, false, true, 60.0);
-        Dragon nobody = new Dragon();
+        //Dragon nobody = new Dragon();
         Slime george = new Slime("George", 10, 15, 6, 20.35, true, "blue", true);
         //Slime nobody2 = new Slime();
-        //Monster drac = new Monster("Drac", 15, 100, 5, 19.45, true, 3, "air");
-        Monster beast = new Monster("beast", 10, 30, 4, 7, true, 4, "land");
-        Monster nobody3 = new Monster();
-        Monster somebody = new Monster(4);
+        Monster drac = new Monster("Drac", 15, 100, 5, 19.45, true, 3, "air");
+        //Monster beast = new Monster("beast", 10, 30, 4, 7, true, 4, "land");
+        //Monster nobody3 = new Monster();
+        //Monster somebody = new Monster(4);
+        Sorcerer me = new Sorcerer("Me", 10, 20, 200, 4, false, "dagger");
+        Sorcerer healer = new Sorcerer(true, "staff");
+        Sorcerer nobody4 = new Sorcerer();
         //System.out.println(nobody3.compareTerrain());
         //happy.setWingspan(-5);
 
-        System.out.printf("Watch out, there are %d enemies attacking %n%n", Enemy.countEnemies);
+        //System.out.printf("Watch out, there are %d enemies attacking %n%n", Enemy.countEnemies);
         //creating array lists
-
+/*
         ArrayList<Enemy> enemyList = new ArrayList<>();
         enemyList.add(happy);
         enemyList.add(george);
@@ -29,6 +32,8 @@ public class MagicDriver
         {
             System.out.println(enemyList.get(i));
         }
+
+ */
         //Enemy[] enemies = {happy, george, beast};
   /*      for(Enemy e: enemies)
         {
@@ -38,6 +43,8 @@ public class MagicDriver
 
    */
         //nobody.setName("Sad");
+
+
         //print out the object via toString
        // System.out.println(happy.toString());
        // System.out.println(nobody.toString());
@@ -46,11 +53,18 @@ public class MagicDriver
         //System.out.println(nobody3.toString());
         //System.out.println(somebody.toString());
         //System.out.println("Nobody has been given a name! It's " + nobody.getName());
+        System.out.println(me.toString());
+        System.out.println(healer.toString());
+        System.out.println(nobody4.toString());
+
         //nobody.setMight(10);
         //drac.setLevel(5);
         //System.out.println(drac.toString());
-        happy.setHealth(happy.getHealth() - beast.attack(1));
-        System.out.println(beast.getName() + " is attacking " + happy.getName() + "! " + happy.getName() + " now has : " + happy.getHealth() + " health");
+
+        //testing attack mechanic
+
+        //happy.setHealth(happy.getHealth() - beast.attack(1));
+        //System.out.println(beast.getName() + " is attacking " + happy.getName() + "! " + happy.getName() + " now has : " + happy.getHealth() + " health");
 
 
     }//end main method
---
+++ src/Sorcerer.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+public class Sorcerer
+{
+    //instance variables
+    int physMight;
+    double magicPower;
+    int health;
+    int movement;
+    boolean hasHeal;
+    String name;
+    ArrayList<String> inventory = new ArrayList<>();
+    //constructors
+    public Sorcerer(String name, int might, double magicMight, int health, int movement, boolean hasHeal, String startingWeapon)
+    {
+        this.name = name;
+        this.physMight = might;
+        this.magicPower = magicMight;
+        this.health = health;
+        this.movement = movement;
+        this.hasHeal = hasHeal;
+        inventory.add(startingWeapon);
+    }//end of full constructor
+    public Sorcerer()
+    {
+        name = "Sam Smorkle";
+        physMight = 0;
+        this.magicPower = 0;
+        this.hasHeal = false;
+        health = 0;
+        movement = 0;
+        inventory.add("shank");
+    }//end default constructor
+    public Sorcerer(String name)
+    {
+        this.name = name;
+        physMight = 0;
+        this.magicPower = 0;
+        this.hasHeal = false;
+        health = 0;
+        movement = 0;
+        inventory.add("shank");
+    }//end constructor with only name different
+    public Sorcerer(boolean hasHeal, String startingWeapon)
+    {
+        this. hasHeal = hasHeal;
+        inventory.add(startingWeapon);
+        this.name = "Sam Smorkle";
+        physMight = 0;
+        this.magicPower = 0;
+        health = 0;
+        movement = 0;
+
+    }//constructor to create pure healer
+
+    //getters
+    public String getInventory()
+    {
+        String output = "";
+        for(String e: inventory)
+        {
+            output += e + ", ";
+        }//end for
+        return output;
+    }//prints out a list of the things in the player's inventory
+    //to string
+    public String toString()
+    {
+        String output = "-----Sorcerer-----";
+        output += "\nName: " + name;
+        output += "\nHealth: " + health;
+        output += "\nMight: " + physMight;
+        output += "\nMagic Power: " + magicPower;
+        output += "\nHealer: " + hasHeal;
+        output += "\nMovement: " + movement;
+        output += "\nInventory: " + getInventory();
+        return output;
+    }//end toString
+
+}//end class sorcerer
